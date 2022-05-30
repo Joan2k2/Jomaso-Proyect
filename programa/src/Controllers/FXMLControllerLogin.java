f902bca2b61d6be89303187470ed66d5ef4966f5
Joan <joruma02@xuqueralumnat.es>
2022-05-30 14:05:35 +0200

    Añadimos para cambiar de escenas(incompelto)

--- programa/src/Controllers/FXMLControllerLogin.java
+++ programa/src/Controllers/FXMLControllerLogin.java
@@ -4,16 +4,23 @@
  */
 package Controllers;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Hyperlink;
 import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -49,14 +56,53 @@ public class FXMLControllerLogin implements Initializable {
 
     @FXML
     private void loginPassw(ActionEvent event) {
+        
     }
 
     @FXML
     private void llevaaregistrarse(ActionEvent event) {
+         try {
+                    
+                    FXMLLoader loader = new FXMLLoader(getClass().getResource("/Vistas/FXMLRegistrarse.fxml"));
+
+                    Parent root = loader.load();
+
+                    Scene scene = new Scene(root);
+                    Stage stage = new Stage();
+
+                    stage.setScene(scene);
+                    stage.show();
+
+                    Stage myStage = (Stage) this.botonLogin.getScene().getWindow();
+                    myStage.close();
+
+                } catch (IOException ex) {
+                    Logger.getLogger(FXMLRegistrarseController.class.getName()).log(Level.SEVERE, null, ex);
+                }
     }
 
     @FXML
     private void logearte(ActionEvent event) {
+        try {
+
+                     FXMLLoader loader = new FXMLLoader(getClass().getResource("/Vistas/FXMLRegistrarse.fxml"));
+
+                     Parent root = loader.load();
+
+                     Scene scene = new Scene(root);
+                     Stage stage = new Stage();
+
+                     stage.setScene(scene);
+                     stage.show();
+
+                     Stage myStage = (Stage) this.botonLogin.getScene().getWindow();
+                     myStage.close();
+
+                 } catch (IOException ex) {
+                     Logger.getLogger(FXMLRegistrarseController.class.getName()).log(Level.SEVERE, null, ex);
+                 }
+        
+        
     }
     
 }
