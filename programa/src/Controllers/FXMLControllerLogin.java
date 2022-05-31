45f2f41730291be0668e8ceec6672aed4bf06ac5
Joan <joruma02@xuqueralumnat.es>
2022-05-31 11:26:39 +0200

    no message

--- programa/src/Controllers/FXMLControllerLogin.java
+++ programa/src/Controllers/FXMLControllerLogin.java
@@ -15,6 +15,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Hyperlink;
 import javafx.scene.control.TextField;
@@ -50,14 +51,6 @@ public class FXMLControllerLogin implements Initializable {
         // TODO
     }    
 
-    @FXML
-    private void loginUsr(ActionEvent event) {
-    }
-
-    @FXML
-    private void loginPassw(ActionEvent event) {
-        
-    }
 
     @FXML
     private void llevaaregistrarse(ActionEvent event) {
@@ -83,9 +76,15 @@ public class FXMLControllerLogin implements Initializable {
 
     @FXML
     private void logearte(ActionEvent event) {
+        
+        
+        
+        
+        
+        
         try {
 
-                     FXMLLoader loader = new FXMLLoader(getClass().getResource("/Vistas/FXMLRegistrarse.fxml"));
+                     FXMLLoader loader = new FXMLLoader(getClass().getResource("/Vistas/FXMLHome.fxml"));
 
                      Parent root = loader.load();
 
