c62bc23073d0b1fb65ae954507de9bd1b29ff81c
Joan <joruma02@xuqueralumnat.es>
2022-06-02 14:08:01 +0200

    elimino mi funcion porque se puede hacer mas facil en bdd

--- programa/src/model/Usuario.java
+++ programa/src/model/Usuario.java
@@ -163,27 +163,6 @@ public class Usuario {
         this.img = img;
     }
 
-    /**
-     * Metodo para encriptar la contraseña intoducida por el Usuario a MD5
-     * (devuelve un string)
-     * @author Joan Ruiz
-     * @param "String"
-    */
-    public static String converMD5(String input) {
- try {
- MessageDigest md = MessageDigest.getInstance("MD5");
- byte[] messageDigest = md.digest(input.getBytes());
- BigInteger number = new BigInteger(1, messageDigest);
- String hashtext = number.toString(16);
-
- while (hashtext.length() < 32) {
- hashtext = "0" + hashtext;
- }
- return hashtext;
- }
- catch (NoSuchAlgorithmException e) {
- throw new RuntimeException(e);
- }
- }
+    
     
 }
